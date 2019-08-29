2f15ec2294622f8b67aaab675b88aa395f7c186f
luguian <luga@chetong.net>
2019-08-29 17:39:59 +0800

    第一次提交

---
+++ app/src/main/java/com/knight/forcecastmvvm/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.knight.forcecastmvvm.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.knight.forcecastmvvm.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
